0b437fb1a818817028280e9fc5bcb3c32da1bedf
Andre P Oliveira <andresystems95@gmail.com>
2017-07-26 15:08:29 -0300

    App
    App

---
+++ www/js/controllers.js
@@ -0,0 +1,199 @@
+angular.module('app.controllers', [])
+  
+.controller('marketPlaceCtrl', ['$scope', '$stateParams', '$ionicUser', '$ionicAuth', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $ionicUser, $ionicAuth, $state) {
+    
+    $scope.userData = $ionicUser.details;
+    
+}])
+   
+.controller('cartCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('artificialIntelligenceCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('applicationsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloudCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('devOpsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', '$ionicUser', '$ionicAuth', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $ionicUser, $ionicAuth, $state) {
+    
+    $scope.userData = $ionicUser.details;
+
+    $scope.logout = function(){
+        $ionicAuth.logout();
+        $state.go('menu.login');
+    }
+    
+    $scope.autenticated = function(){
+        if ($ionicAuth.isAuthenticated()) {
+        // Make sure the user data is going to be loaded
+        return true;
+        }else{
+        return null
+        }
+        
+    }
+
+}])
+   
+.controller('futureOfAICtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloud2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloudTechnologyBringsNewRolesForITCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('aREFEDERALAGENCIESCLOUDREADYCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('dEVOPSAREYOUREADYTOSHIFTTOCONTINUOUSDELIVERYCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('whatSInANameCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('mAKINGDEVOPSATSCALESTICKCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', '$ionicUser', '$ionicAuth', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $ionicUser, $ionicAuth, $state) {
+
+    $scope.data = {
+        'email': '',
+        'password': ''
+    }
+    
+    $scope.error = '';
+    
+    if ($ionicAuth.isAuthenticated()) {
+        // Make sure the user data is going to be loaded
+        $ionicUser.load().then(function() {});
+        $state.go('menu.marketPlace'); 
+    }
+    
+    
+    $scope.login = function(){
+        $scope.error = '';
+        $ionicAuth.login('basic', $scope.data).then(function(){
+            $state.go('menu.marketPlace');
+        }, function(){
+            $scope.error = 'Error logging in.';
+        })
+    }
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', '$ionicAuth', '$ionicUser', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $ionicAuth, $ionicUser, $state) {
+    
+    $scope.data = {
+        'name': '',
+        'email': '',
+        'password': ''
+    }
+    
+    $scope.error='';
+
+    $scope.signup = function(){
+        
+        $scope.error = '';
+
+        $ionicAuth.signup($scope.data).then(function() {
+            // `$ionicUser` is now registered
+            $ionicAuth.login('basic', $scope.data).then(function(){
+              $state.go('menu.marketPlace');
+            });
+        }, function(err) {
+            
+            var error_lookup = {
+                'required_email': 'Missing email field',
+                'required_password': 'Missing password field',
+                'conflict_email': 'A user has already signed up with that email',
+                'conflict_username': 'A user has already signed up with that username',
+                'invalid_email': 'The email did not pass validation'
+            }    
+        
+            $scope.error = error_lookup[err.details[0]];
+        });
+    }
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,171 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('menu.marketPlace', {
+    url: '/page3',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/marketPlace.html',
+        controller: 'marketPlaceCtrl'
+      }
+    }
+  })
+
+  .state('menu.cart', {
+    url: '/page2',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cart.html',
+        controller: 'cartCtrl'
+      }
+    }
+  })
+
+  .state('menu.artificialIntelligence', {
+    url: '/page4',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/artificialIntelligence.html',
+        controller: 'artificialIntelligenceCtrl'
+      }
+    }
+  })
+
+  .state('menu.applications', {
+    url: '/page8',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/applications.html',
+        controller: 'applicationsCtrl'
+      }
+    }
+  })
+
+  .state('menu.cloud', {
+    url: '/page6',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cloud.html',
+        controller: 'cloudCtrl'
+      }
+    }
+  })
+
+  .state('menu.devOps', {
+    url: '/page5',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/devOps.html',
+        controller: 'devOpsCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.futureOfAI', {
+    url: '/page9',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/futureOfAI.html',
+        controller: 'futureOfAICtrl'
+      }
+    }
+  })
+
+  .state('menu.cloud2', {
+    url: '/page10',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cloud2.html',
+        controller: 'cloud2Ctrl'
+      }
+    }
+  })
+
+  .state('menu.cloudTechnologyBringsNewRolesForIT', {
+    url: '/page11',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cloudTechnologyBringsNewRolesForIT.html',
+        controller: 'cloudTechnologyBringsNewRolesForITCtrl'
+      }
+    }
+  })
+
+  .state('menu.aREFEDERALAGENCIESCLOUDREADY', {
+    url: '/page12',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/aREFEDERALAGENCIESCLOUDREADY.html',
+        controller: 'aREFEDERALAGENCIESCLOUDREADYCtrl'
+      }
+    }
+  })
+
+  .state('menu.dEVOPSAREYOUREADYTOSHIFTTOCONTINUOUSDELIVERY', {
+    url: '/page13',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/dEVOPSAREYOUREADYTOSHIFTTOCONTINUOUSDELIVERY.html',
+        controller: 'dEVOPSAREYOUREADYTOSHIFTTOCONTINUOUSDELIVERYCtrl'
+      }
+    }
+  })
+
+  .state('menu.whatSInAName', {
+    url: '/page14',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/whatSInAName.html',
+        controller: 'whatSInANameCtrl'
+      }
+    }
+  })
+
+  .state('menu.mAKINGDEVOPSATSCALESTICK', {
+    url: '/page15',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/mAKINGDEVOPSATSCALESTICK.html',
+        controller: 'mAKINGDEVOPSATSCALESTICKCtrl'
+      }
+    }
+  })
+
+  .state('menu.login', {
+    url: '/page16',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('menu.signup', {
+    url: '/page17',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/signup.html',
+        controller: 'signupCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/page3')
+
+
+});
